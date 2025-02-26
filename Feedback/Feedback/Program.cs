99d09822df49323d8c084779c7c993ef7c2f3e33
Aaron <hansenag02@gmail.com>
2025-02-26 17:11:15 -0600

    FluentAnchor feedback.

---
+++ Feedback/Feedback/Program.cs
@@ -0,0 +1,37 @@
+﻿using Feedback.Client.Pages;
+using Feedback.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddFluentUIComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Feedback.Client._Imports).Assembly);
+
+app.Run();
